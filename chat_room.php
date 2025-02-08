0c99ff64638970986c423194435d7fd102edef77
IndhuJoy-Sodadasi <n210837@rguktn.ac.in>
2025-02-08 17:33:05 +0530

    chat page changes

--- chat_room.php
+++ chat_room.php
@@ -101,7 +101,6 @@ if (!$chat_details) {
             text-decoration: underline;
         }
     </style>
-    <div id="google_translate_element"></div>
 </head>
 <body>
     <div class="container">
@@ -152,16 +151,5 @@ if (!$chat_details) {
             </div>
         <?php endif; ?>
     </div>
-    <script src="script.js"></script>
-      <script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement(
-                {pageLanguage: 'en'},
-                'google_translate_element'
-            );
-        } 
-  </script>
-  <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
 </body>
 </html>
